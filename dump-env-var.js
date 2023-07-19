9af6ec4024aac168a28770836bba11bf794a68b0
Matteo Rigon <matteo.rigon7@gmail.com>
2023-07-19 13:20:59 +0200

    first commit

---
+++ dump-env-var.js
@@ -0,0 +1,7 @@
+console.log('');
+console.log('------------------------------------');
+console.log('');
+console.log(` process.env.MY_BUILD_ENV_VAR is "${process.env.MY_BUILD_ENV_VAR}"`);
+console.log('');
+console.log('------------------------------------');
+console.log('');
